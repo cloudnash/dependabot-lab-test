dfb070d45f4bdffd5bccf1116d0884683314b10b
Nashit Ahmad <fusionmad47@gmail.com>
2026-06-27 22:36:06 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// Minimal entry point so the npm manifest references real dependencies.
-const _ = require("lodash");
-
-function main() {
-  console.log(_.capitalize("dependabot lab ready"));
-}
-
-main();
